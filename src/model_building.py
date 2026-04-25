638624804c7f6a246af54b5317a79e22d9c37d3b
AhmadTigress <tigressfx@gmail.com>
2026-04-25 10:51:59 +0100

    Adding my files

---
+++ src/model_building.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import os
+
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+
+train_data = pd.read_csv("./data/processed/train_processed.csv")
+
+X_train = train_data.drop(['Potability'], axis=1)
+y_train = train_data['Potability']
+
+clf = RandomForestClassifier()
+clf.fit(X_train, y_train)
+
+pickle.dump(clf, open("model.pkl", "wb"))
